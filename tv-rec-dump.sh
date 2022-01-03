9578f4daf8fcc8e6eed6d25a3753c8628f0d9f94
smallbeetw <smallbee.tw@gmail.com>
2022-01-03 11:24:39 +0800

    tv-rec-dump: change gpio procedure extend sleep time after power off

--- tv-rec-dump.sh
+++ tv-rec-dump.sh
@@ -41,36 +41,31 @@ AverMediaPower()
 	echo -e "P" > $AVERMEDIA_TTY
 }
 
+# Raspberry Pi be connected to the normal open side of Relay
+# AverMedia ER130 be conntected to the normal close side of Relay
+# And this is a Low level trigger Relay
 switchUSB2Rasp()
 {
-	# low gpio to enable power of relay
-#	gpioset gpiochip0 14=0 15=0
-#	sleep 1
-	# low gpio to detach USB pins of AverMedia
-#	gpioset gpiochip0 2=0 3=0 4=0 5=0
-	# attach: power pin first, then data pin
-#	gpioset gpiochip0 4=0
-#	sleep 1
-#	gpioset gpiochip0 3=0
-
-	# pin 3 : D+ D- data pin
-	# pin 4 : power/ground pin
-	# pin 17 : second level power/ground pin
-	gpioset gpiochip0 4=0; sleep 1; gpioset gpiochip0 17=1; sleep 1; gpioset gpiochip0 3=0
+	# Attach logic from USB: Power pin first, then Data pin
+	# pin 3 : D+ D- data pin	be controlled by a low-level trigger Relay
+	# pin 4 : power/ground pin	be controlled by a low-level trigger Relay
+	# pin 17 : second level power/ground pin control, be controlled by a MOSFET
+	 gpioset gpiochip0 17=0; sleep 1; gpioset gpiochip0 4=0; sleep 1; gpioset gpiochip0 17=1; sleep 1; gpioset gpiochip0 3=0
 	echo "switched USB to Raspberry pi"
 }
 
+# A MOSFET be used as the second level controller of power pin. It's to control
+# the timing of enabling the power when attaching to ER130. The VBUS? power of
+# thumb disk must be turn-off at least 0.5-1 second before the thumb be attached
+# on ER130 or another machine. Otherwise that the thumb can not be initial by ER130
+# or anyother machine. Without a second-level power control (MOSFET here), only
+# one Relay can not fulfill this requirement.
 switchUSB2AverMedia()
 {
-	# high gpio to detach USB pins of Raspberry Pi
-#	gpioset gpiochip0 2=1 3=1 4=1 5=1
-#	sleep 1
-	# high gpio to disable power of relay
-#	gpioset gpiochip0 14=1 15=1
-	# detach: data pin first, then power pin
-#	gpioset gpiochip0 3=1
-#	sleep 1
-#	gpioset gpiochip0 4=1
+	# Dttach logic from USB: Data pin first, then Power pin
+	# pin 3 : D+ D- data pin	be controlled by a low-level trigger Relay
+	# pin 4 : power/ground pin	be controlled by a low-level trigger Relay
+	# pin 17 : second level power/ground pin control, be controlled by a MOSFET
 	gpioset gpiochip0 3=1; sleep 1; gpioset gpiochip0 17=0; sleep 1; gpioset gpiochip0 4=1; sleep 1; gpioset gpiochip0 17=1
 	echo "switched USB to AverMedia"
 }
@@ -115,7 +110,7 @@ dump()
 detachAvermedia
 
 AverMediaPower
-sleep 5
+sleep 20
 
 switchUSB2Rasp
 sleep 5
