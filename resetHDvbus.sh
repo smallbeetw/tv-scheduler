28f68fbdf869227a893791debe8b2e60b38217bd
smallbeetw <smallbee.tw@gmail.com>
2024-10-27 17:06:53 +0800

    resetHDvbus: Reset the power state of ER-130 disk
    Sometimes ER-130 picks up USB disk fail when power on. This
    script tool runs reset the power state of disk to trigger the
    ER-130 pick up process again:
    
      Turn-off AverMedia ER-130 Box.
      Power-off USB disk by switch vbus to normal open state.
      Power-on USB disk by switch vbus back to normal close state.
      Turn-on AverMedia ER-130 Box. ER-130 auto-picks up USB disk.

---
+++ resetHDvbus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source tv-scheduler.conf
+source tv-rec-post-utils.sh
+
+echo "Turn off AverMedia ER130"
+AverMediaPower
+
+# Turn off power pin 1 second for reset vbus 
+echo "Turn off vbus"
+gpioset gpiochip0 17=1; sleep 5
+
+# Turn on power pin
+echo "Turn on vbus"
+gpioset gpiochip0 17=0; sleep 5
+
+echo "Turn on AverMedia ER130"
+AverMediaPower
