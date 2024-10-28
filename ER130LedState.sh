36898a03df5223e04f092445ff1d8e1ca03d251a
smallbeetw <smallbee.tw@gmail.com>
2024-10-28 15:43:21 +0800

    ER130LedState.sh: checking the LED state of ER130
    I put one photoresistor to montior the state LED of
    ER130. It's useful for remote checking the status
    of ER130. It's also a debuggint tool for auto-reset
    vbus of USB HD for recording (HD power reset).

---
+++ ER130LedState.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source tv-scheduler.conf
+source tv-rec-post-utils.sh
+
+sampling()
+{
+    for i in {0..9};
+    do
+        echo -e "?" > $AVERMEDIA_TTY
+        sleep 1
+        read -N1 LED_VALUE < /dev/ttyUSB0
+        samples[i]=$LED_VALUE
+        echo ${samples[$i]}
+    done
+}
+
+
+ledConstantlyBright()
+{
+    CONSTANT=true
+    sample0=${samples[0]}
+    for i in ${samples[@]};
+    do
+        if [[ "$sample0" != "$i" ]]; then
+            CONSTANT=false
+            break
+        fi
+    done
+    if [ $CONSTANT == true ]; then
+	TVSCH_FILE=$TVSCH_FILE"B"
+        echo "Constantly Bright"
+    else
+        echo "NOT Constantly Bright"
+    fi
+}
+
+# ledTwinkling
+
+# ledBreathing
+
+sampling
+
+ledConstantlyBright
