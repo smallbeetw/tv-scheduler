90123c6c7224426ec5e8bf93303ae40e491d717c
smallbeetw <smallbee.tw@gmail.com>
2025-01-12 21:37:49 +0800

    ER130-utils: create utilities file for controlling AverMedia ER130
    Collected helper functions in ER130-utils for controlling AVerMedia
    ER130 recording box. The first two functions are ledSampling() and
    ledConstantlyGreenBright(). The ledSampling() is for sampling LED
    states on ER130 through photoresistor. The ledConstantlyGreenBright()
    is a parser for checking those samples to confirm that the LED is in
    constantly green bright state. The CONSTANTGREEN variable will be set
    to 'true'.

---
+++ ER130-utils.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source tv-scheduler.conf
+source tv-rec-post-utils.sh
+
+ledSampling()
+{
+    for i in {0..9};
+    do
+        echo -e "?" > $AVERMEDIA_TTY
+        sleep 1
+        read -N1 LED_VALUE < $AVERMEDIA_TTY
+        samples[i]=$LED_VALUE
+        echo ${samples[$i]}
+    done
+}
+
+
+ledConstantlyGreenBright()
+{
+    CONSTANTGREEN=false
+    CONSTANT=true
+    sample0=${samples[0]}
+    for i in ${samples[@]};
+    do
+        if [[ "$sample0" != "$i" ]]; then
+            CONSTANT=false
+            break
+        fi
+    done
+    if [ $CONSTANT == true ]; then
+	CONSTANTGREEN=true
+    else
+        printLog "NOT Constantly Green Bright"
+    fi
+}
