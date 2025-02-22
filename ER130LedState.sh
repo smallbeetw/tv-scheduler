72bbc3818b4fffa3c242e326691d0b899865793a
smallbeetw <smallbee.tw@gmail.com>
2025-02-22 19:38:09 +0800

    ER130LedState: Add helpers for green flashes and constantly amber bright
    Add helpers for detecting green flashes and constantly amber bright
    states. And add samples for testing.

--- ER130LedState.sh
+++ ER130LedState.sh
@@ -15,7 +15,13 @@ ledSampling()
     done
 }
 
-
+# Patterns of sampling:
+#   Constantly Green Bright:
+#     LED samples: 4 4 4 4 4 4 4 4 4 4
+#     LED samples: 5 4 4 4 4 4 4 4 4 4
+#   NOT Constantly Green Bright
+#     LED samples: 4 4 4 4 4 4 4 5 0 0
+#     LED samples: 0 0 0 4 0 0 0 0 0 2
 ledConstantlyGreenBright()
 {
     CONSTANT=true
@@ -34,10 +40,111 @@ ledConstantlyGreenBright()
     fi
 }
 
+# Patterns of sampling:
+#   Green Flashes:
+#     LED samples: 4 4 4 4 4 4 4 5 0 0
+#     LED samples: 0 0 0 4 0 0 0 0 0 2
+#     LED samples: 6 0 0 0 0 0 6 0 0 0
+#     LED samples: 5 5 0 5 5 1 5 5 0 5
+#   The lowest value is 0 or 1
+#   Difference between highest and lowest values >= 4
+ledGreenFlashing()
+{
+    GREENFLASHING=false
+    lowest=9
+    highest=0
+    sample0=${samples[0]}
+    for i in ${samples[@]};
+    do
+	if [ $i -lt $lowest ]; then
+		lowest=$i
+	fi
+	if [ $i -gt $highest ]; then
+		highest=$i
+	fi
+    done
+    difference=`expr $highest - $lowest`
+    if [[ $lowest -le 1 ]] && [[ $difference -ge 4 ]]; then
+	GREENFLASHING=true
+	printLog "Green Flashing"
+	echo "Green Flashing"
+    else
+	printLog "Not Green Flashing"
+	echo "Not Green Flashing"
+    fi
+    echo "lowest "$lowest
+    echo "difference "$difference
+}
+
+# Patterns of sampling:
+#   Constantly Amber Bright:
+#     LED samples: 5 7 6 6 5 5 5 4 5 4
+#     LED samples: 5 4 8 7 7 6 6 5 5 4
+#     LED samples: 6 5 5 7 6 5 6 7 5 4
+#     LED samples: 6 6 6 9 7 5 4 4 7 8
+#     LED samples: 5 4 7 8 6 5 6 6 6 6
+#   There are four or five consecutive numbers
+#   No 0 or 1 value
+ledConstantlyAmberBright()
+{
+    IFS=$'\n' sorted=($(sort <<<"${samples[*]}"))
+    unset IFS
+
+    echo "sorted samples: ${sorted[*]}"
+    CONSTANTAMBER=false
+    consecutive=1
+    for ((index=0; index < ${#sorted[@]}; index++)); do
+	# If 0 or 1 be found, then it's NOT a sample of constantly amber
+	if [ ${sorted[index]} -le 1 ]; then
+	    break
+	fi
+	# if (i+1) - i = 1, then they are consecutive
+	if [ $(($index+1)) -lt ${#sorted[@]} ]; then
+	    if [ ${sorted[index+1]} -gt ${sorted[index]} ]; then
+		difference=`expr ${sorted[index+1]} - ${sorted[index]}`
+		if [ $difference -eq 1 ]; then
+		    consecutive=$(($consecutive+1))
+		fi
+	    fi
+	fi
+    done
+    # consecutive >= 4 means the samples have 4 consecutive numbers at least
+    if [ $consecutive -ge 4 ]; then
+	CONSTANTAMBER=true
+	printLog "Constantly Amber"
+	echo "Constantly Amber"
+    else
+	printLog "Not Contantly Amber"
+	echo "Not Contantly Amber"
+    fi
+    echo "consecutive: "$consecutive
+}
+
 # ledTwinkling
 
 # ledBreathing
 
 ledSampling
 
+# Constantly Green Bright:
+#samples=(4 4 4 4 4 4 4 4 4 4)
+#samples=(5 4 4 4 4 4 4 4 4 4)
+
+# Green Flashes:
+#samples=(4 4 4 4 4 4 4 5 0 0)
+#samples=(0 0 0 4 0 0 0 0 0 2)
+#samples=(6 0 0 0 0 0 6 0 0 0)
+#samples=(5 5 0 5 5 1 5 5 0 5)
+
+# Constantly Amber Bright:
+#samples=(5 7 6 6 5 5 5 4 5 4)
+#samples=(5 4 8 7 7 6 6 5 5 4)
+#samples=(6 5 5 7 6 5 6 7 5 4)
+#samples=(6 6 6 9 7 5 4 4 7 8)
+#samples=(5 4 7 8 6 5 6 6 6 6)
+
 ledConstantlyGreenBright
+
+ledGreenFlashing
+
+ledConstantlyAmberBright
